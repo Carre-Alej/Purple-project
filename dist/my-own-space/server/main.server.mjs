a26dccfcfc9b71c9568a83a0290ba2284a7a505e
Alejandro Carrero <alejandroyroky@gmail.com>
2024-09-15 17:29:09 +0200

    cambios y agregando /dist

---
+++ dist/my-own-space/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-BVVUEAWR.mjs";import"./chunk-EMELZ6IC.mjs";import"./chunk-5T2EDLQI.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule, MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
+import { MatMenu } from '@angular/material/menu'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatToolbarModule, MatIcon], 
+  imports: [RouterOutlet, MatToolbar, MatToolbarModule, MatIcon, MatMenu], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
