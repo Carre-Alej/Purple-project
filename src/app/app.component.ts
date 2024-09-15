d829b0fdf1b846bd12da484509ca42d9e36daaf7
Alejandro Carrero <alejandroyroky@gmail.com>
2024-09-15 09:37:10 +0200

    Comprobación de lectura index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule, MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatToolbarModule], 
+  imports: [RouterOutlet, MatToolbar, MatToolbarModule, MatIcon], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
